ffecab91d8df5b21dedd3854cc4b743f3a9a9a6f
Michael Mayer <michael@schnittstabil.de>
2016-02-24 14:52:44 +0100

    fix style

--- gulpfile.js
+++ gulpfile.js
@@ -1,63 +1,30 @@
 'use strict';
-var gulp = require('gulp'),
-    istanbul = require('gulp-istanbul'),
-    jscs = require('gulp-jscs'),
-    jshint = require('gulp-jshint'),
-    mocha = require('gulp-mocha'),
-    scripts = ['**/*.js', '!node_modules/**', '!coverage/**'],
-    tests = 'test.js';
+var gulp = require('gulp');
+var istanbul = require('gulp-istanbul');
+var mocha = require('gulp-mocha');
 
-function lint(fail) {
-  return function() {
-    var l = gulp.src(scripts)
-        /* hint */
-      .pipe(jshint())
-      .pipe(jshint.reporter())
-      /* jscs */
-      .pipe(jscs());
+var scripts = ['**/*.js', '!node_modules/**', '!coverage/**'];
+var tests = 'test.js';
 
-    if (fail) {
-      return l.pipe(jshint.reporter('fail'));
-    } else {
-      return l;
-    }
-  };
-}
-
-gulp.task('lint', lint(true));
-gulp.task('lint-watch', lint(false));
-
-gulp.task('watch:lint', function() {
-  gulp.watch(scripts, ['lint-watch']);
+gulp.task('test', function () {
+	return gulp.src(tests)
+		.pipe(mocha({reporter: 'spec'}));
 });
 
-gulp.task('watch:test', function() {
-  gulp.watch(scripts, ['test']);
+gulp.task('instrument', function () {
+	return gulp.src(scripts.concat(['!' + tests]))
+		.pipe(istanbul())
+		.pipe(istanbul.hookRequire());
 });
 
-gulp.task('watch', function() {
-  gulp.watch(scripts, ['lint-watch', 'test']);
+gulp.task('coverage', ['instrument'], function () {
+	return gulp.src(tests)
+		.pipe(mocha({
+			reporter: 'dot'
+		}))
+		.pipe(istanbul.writeReports({
+			reporters: ['lcovonly', 'text-summary', 'html']
+		}));
 });
 
-gulp.task('test', function() {
-  return gulp.src(tests)
-      .pipe(mocha({reporter: 'spec'}));
-});
-
-gulp.task('instrument', function() {
-  return gulp.src(scripts.concat(['!' + tests]))
-    .pipe(istanbul())
-    .pipe(istanbul.hookRequire());
-});
-
-gulp.task('coverage', ['instrument'], function() {
-  return gulp.src(tests)
-    .pipe(mocha({
-      reporter: 'dot'
-    }))
-    .pipe(istanbul.writeReports({
-      reporters: ['lcovonly', 'text-summary', 'html']
-    }));
-});
-
-gulp.task('default', ['lint', 'coverage']);
+gulp.task('default', ['coverage']);
--- index.js
+++ index.js
@@ -1,41 +1,41 @@
 'use strict';
-var Readable = require('stream').Readable,
-    inherits = require('util').inherits;
+var Readable = require('stream').Readable;
+var inherits = require('util').inherits;
 
 function StreamFromPromise(promise, options) {
-  if (!(this instanceof StreamFromPromise)) {
-    return new StreamFromPromise(promise, options);
-  }
+	if (!(this instanceof StreamFromPromise)) {
+		return new StreamFromPromise(promise, options);
+	}
 
-  Readable.call(this, options);
+	Readable.call(this, options);
 
-  this.__promise = promise;
-  this.__resolvingPromise = false;
+	this.__promise = promise;
+	this.__resolvingPromise = false;
 }
 inherits(StreamFromPromise, Readable);
 
-StreamFromPromise.obj = function(promise, options) {
-  options = options || {};
-  options.objectMode = true;
-  return new StreamFromPromise(promise, options);
+StreamFromPromise.obj = function (promise, options) {
+	options = options || {};
+	options.objectMode = true;
+	return new StreamFromPromise(promise, options);
 };
 
-StreamFromPromise.prototype._read = function() {
-  var self;
-  if (!this.__resolvingPromise) {
-    this.__resolvingPromise = true;
-    self = this;
-    this.__promise.then(
-      function(value) {
-        self.push(value);
-        self.push(null);
-      },
-      function(reason) {
-        self.emit('error', reason);
-        self.push(null);
-      }
-    );
-  }
+StreamFromPromise.prototype._read = function () {
+	var self;
+	if (!this.__resolvingPromise) {
+		this.__resolvingPromise = true;
+		self = this;
+		this.__promise.then(
+			function (value) {
+				self.push(value);
+				self.push(null);
+			},
+			function (reason) {
+				self.emit('error', reason);
+				self.push(null);
+			}
+		);
+	}
 };
 
 module.exports = StreamFromPromise;
